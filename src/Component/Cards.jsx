9e0a41271df94fc69e3e5b312a9ff71514148405
Jahari <jahari.crawford.young@gmail.com>
2024-04-17 10:39:24 -0400

    fresh

--- src/Component/Cards.jsx
+++ src/Component/Cards.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Cards = () => {
   const cardsData = [
-    { id: 1, frontContent: 'L.I.F.E.`s Alternative: L.E.A.D. (Let Everyone Advance with Dignity) Diversion Program', iconClass: ""},
+    { id: 1, frontContent: 'L.I.F.E.`s Alternative: L.E.A.D. (Let Everyone Advance with Dignity) Diversion Program'},
     { id: 2, frontContent: 'Back2LIFE Reintegration & Transition Support Program' },
     { id: 3, frontContent: '1Life2Live Gun & Gang Violence Prevention & Intervention Program' }
   ];
