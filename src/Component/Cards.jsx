59a5de9ed485d08c4d4a8537cdd1c7f3eac116e2
jahari11 <jahari.crawford.young@gmail.com>
2024-06-10 09:44:53 -0400

    6/10 updates

--- src/Component/Cards.jsx
+++ src/Component/Cards.jsx
@@ -7,27 +7,25 @@ const Cards = () => {
 
 
   const cardsData = [
-    { id: 1, frontContent: `L.I.F.E.'s Alternative: L.E.A.D. (Let Everyone Advance with Dignity) Diversion Program`, sectionId:'program-3'},
-    { id: 2, frontContent: 'Back2LIFE Reintegration & Transition Support Program', sectionId: 'program-1' },
-    { id: 3, frontContent: '1Life2Live Gun & Gang Violence Prevention & Intervention Program', sectionId: 'program-2' }
+    { id: 1, frontContent: `L.I.F.E.'s Alternative: L.E.A.D Diversion Program`, sectionId:'program-3', backContent: `Our harm-reduction diversion program's success is based on the Operation Work Group (OWG), Policy Coordinating Group (PCG), and Community Engagement Coordinators.`},
+    { id: 2, frontContent: 'Back2LIFE Reintegration & Transition Support Program', sectionId: 'program-1', backContent: `Back2LIFE Reintegration & Transition Support works with inmates at Schenectady County Jail to reduce recidivism and enhance public safety.` },
+    { id: 3, frontContent: '1Life2Live Gun & Gang Violence Prevention & Intervention Program', sectionId: 'program-2', backContent: `The 1Life2Live program aims to reduce gun violence, shooting injuries, and homicides in Schenectady County over time.` }
   ];
 
-  return (
-    <div className='c-section'>
-    <h2 className='c-section-h'>Our Programs</h2>
-    <div className='card-container'>
-      {cardsData.map(card => (
-        <Link to={`/programs#${card.sectionId}`}>
-        <div className='card' key={card.id}>
-            <i class="fa-regular fa-flag"></i>
-          <h2>{card.frontContent}</h2>
-          <span>Read More</span>
-        </div>
-        </Link>
-      ))}
-    </div>
-    </div>
+  return(
+  <div class='bg-ylsnGrey p-4'>
+  <h2 class='text-center  text-4xl text-white'>Our Programs</h2>
+  <div class='flex justify-between flex-wrap'>
+  {cardsData.map(card => (
+      <Link to={`/programs#${card.sectionId}`} class=" m-6 block max-w-sm p-6 border-gray-200 rounded-lg shadow hover:bg-gray-100 bg-ylsnRed dark:hover:bg-gray-700 flex flex-col min-h-[300px]">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">{card.frontContent}</h5>
+        <p class="font-normal text-lg mb-3 text-white text-center">{card.backContent}</p>
+        <p class='underline text-center text-lg text-white'>Read more</p>
+      </Link>
+    ))}
+  </div>
+  </div>
   );
-};
+}
 
 export default Cards;
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
         <span className='year'>Since 2012</span>
         <h3 className='aup-header'>Youth L.I.F.E. Support Network, Inc.</h3>
         <div className='aup-text'>
-        <p>Youth L.I.F.E. Support Network, Inc. is a community-based 501(c)(3) non-profit organization dedicated to promoting public safety. Embracing a public health model approach, we tackle community violence by providing comprehensive, holistic services to high-risk youth and families in underserved areas. Our mission is to empower youth to thrive and embrace the promise of life.</p>
+        <p>Youth L.I.F.E. Support Network, Inc. is a community-based 501(c)(3) non-profit organization dedicated to promoting public safety. Embracing a public health model approach, we tackle community violence by providing comprehensive, holistic services to at-rich youth and families in high-risk areas. Our mission is to empower youth to thrive and embrace the promise of life.</p>
         <p>Through a range of holistic, wrap-around services, Youth L.I.F.E. Support Network, Inc. offers tailored support to address the diverse needs of at-risk youth and their families. By fostering youth development and fostering strong community collaborations, we establish enduring relationships that serve as a vital support system, contributing to the reduction of violence and suffering in our communities.</p>
         <span>The Youth are the Leaders of tomorrow. We must work collectively to empower and support their visions and feed their ambitions.</span>
         </div>
