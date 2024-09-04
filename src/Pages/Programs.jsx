6e3cd5ea14e02f190fea723544faf2f25c073f68
jahari11 <jahari.crawford.young@gmail.com>
2024-09-04 00:10:40 -0400

    font change

--- src/Pages/Programs.jsx
+++ src/Pages/Programs.jsx
@@ -7,8 +7,8 @@ const Programs = () => {
     <div>
       <div className="a-i-container">
         <img src="https://images.unsplash.com/photo-1552664730-d307ca884978?q=80&w=3600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        <div className="e-p-overlay">
-          <h1>Programs</h1>
+        <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center text-white text-5xl bg-black/50">
+          <h1>Our programs</h1>
         </div>
       </div>
       <ProgramTitles />
